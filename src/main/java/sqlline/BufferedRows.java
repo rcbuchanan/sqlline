cd67b0f67acc0b1176e392e70bc4c2910c8e8cf2
Cliff Buchanan <cbuchanan@maprtech.com>
2014-08-12 13:12:10 -0700

    WIP

--- src/main/java/sqlline/BufferedRows.java
+++ src/main/java/sqlline/BufferedRows.java
@@ -25,6 +25,9 @@ class BufferedRows extends Rows {
 
   private final Iterator<Row> iterator;
 
+  int totalwidth = 80;
+
+
   BufferedRows(SqlLine sqlLine, ResultSet rs) throws SQLException {
     super(sqlLine, rs);
 
@@ -49,20 +52,71 @@ class BufferedRows extends Rows {
     return iterator.next();
   }
 
-  void normalizeWidths() {
-    int[] max = null;
-    for (Row row : list) {
-      if (max == null) {
-        max = new int[row.values.length];
-      }
+  private int[] computeWidths(double[] min, double[] max) {
+    int n = min.length;
+    int[] result = new int[n];
 
-      for (int j = 0; j < max.length; j++) {
-        max[j] = Math.max(max[j], row.sizes[j] + 1);
+    // quadratic column sizing based on 0 < r < 1
+    //    r * r + r * (span[i] - 1) = width[i]
+    // Adding up all "n" columns, this comes to
+    //    n * r * r + r * (totalspan - n) = totalwidth
+    // Quadratic parameters
+    //    a = n
+    //    b = totalspan - n
+    //    c = -screenwidth
+
+    double a = n;
+    double b = -n;
+    double c = -totalwidth;
+    for (int i = 0; i < n; i++) {
+      b += max[i] - min[i];
+    }
+
+    double r = (Math.sqrt(b * b - 4 * a * c) - b) / (2 * a);
+    for (int i = 0; i < n; i++) {
+      result[i] = (int) (r * r + (max[i] - min[i] - 1) * r);
+    }
+
+    return result;
+  }
+
+  void normalizeWidths() {
+    if (list.size() <= 0) {
+      return;
+    }
+
+    int n = list.get(0).values.length;
+    double[] min = new double[n];
+    double[] max = new double[n];
+
+    for (Row row : list) {
+      if (row.isMeta) {
+        for (int j = 0; j < n; j++) {
+          min[j] = row.sizes[j];
+        }
+      } else {
+        for (int j = 0; j < n; j++) {
+          max[j] = Math.max(max[j], row.sizes[j]);
+        }
       }
     }
 
+    double minsum = 0.0;
+    for (int i = 0; i < n; i++) {
+      max[i] = Math.max(min[i], max[i]);
+      minsum += min[i];
+    }
+
+    // scale down min widths if columns are impossible to fit
+    if (minsum > totalwidth) {
+      for (int i = 0; i < n; i++) {
+        min[i] *= totalwidth / minsum;
+      }
+    }
+
+    int[] rv = computeWidths(min, max);
     for (Row row : list) {
-      row.sizes = max;
+      row.sizes = rv;
     }
   }
 }
